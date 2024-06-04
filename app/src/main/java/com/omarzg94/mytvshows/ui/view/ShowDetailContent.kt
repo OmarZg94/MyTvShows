7f230ef4ebcec6b03bd743c0ea3090dfff3d99bb
Omar Zúñiga <omar.alejandro.94@outlook.com>
2024-06-04 17:13:50 -0600

    Parse the html content of each episode summary.

--- app/src/main/java/com/omarzg94/mytvshows/ui/view/ShowDetailContent.kt
+++ app/src/main/java/com/omarzg94/mytvshows/ui/view/ShowDetailContent.kt
@@ -1,6 +1,9 @@
 package com.omarzg94.mytvshows.ui.view
 
-import android.util.Log
+import android.graphics.text.LineBreaker.JUSTIFICATION_MODE_INTER_WORD
+import android.view.ViewGroup.LayoutParams
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -8,7 +11,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -20,11 +22,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
 import coil.compose.rememberAsyncImagePainter
+import com.omarzg94.mytvshows.R
 import com.omarzg94.mytvshows.data.model.Episode
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -51,38 +59,65 @@ fun ShowDetailContent(episode: Episode) {
                     .fillMaxSize()
                     .background(Color.Gray)
             )
-            Text(
+            Column(
                 modifier = Modifier
+                    .fillMaxWidth()
                     .align(Alignment.BottomStart)
-                    .padding(horizontal = 16.dp, vertical = 8.dp),
-                text = episode.show.name,
-                style = MaterialTheme.typography.headlineLarge,
-                color = Color.White,
-            )
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            )
+                        )
+                    )
+            ) {
+                Text(
+                    modifier = Modifier.padding(16.dp),
+                    text = episode.show.name,
+                    style = MaterialTheme.typography.headlineLarge,
+                    color = Color.White,
+                )
+            }
         }
         Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            modifier = Modifier.padding(horizontal = 16.dp),
-            text = episode.show.summary ?: "No summary available",
-            style = MaterialTheme.typography.bodyMedium,
-            color = Color.White,
-            textAlign = TextAlign.Justify
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            modifier = Modifier.padding(horizontal = 16.dp),
-            text = "Genres",
-            style = MaterialTheme.typography.titleMedium,
-            color = Color.White
-        )
+        if (!episode.show.summary.isNullOrEmpty()) {
+            AndroidView(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                factory = { context ->
+                    TextView(context).apply {
+                        text =
+                            HtmlCompat.fromHtml(
+                                episode.show.summary,
+                                HtmlCompat.FROM_HTML_MODE_COMPACT
+                            ).trim()
+                        setTextColor(ContextCompat.getColor(context, R.color.header_color))
+                        justificationMode = JUSTIFICATION_MODE_INTER_WORD
+                    }
+                })
+        } else {
+            Text(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                text = stringResource(id = R.string.without_summary),
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.White,
+                textAlign = TextAlign.Justify
+            )
+        }
         if (!episode.show.genres.isNullOrEmpty()) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                text = stringResource(id = R.string.genres),
+                style = MaterialTheme.typography.titleMedium,
+                color = Color.White
+            )
             Spacer(modifier = Modifier.height(8.dp))
             FlowRow(
                 modifier = Modifier.padding(horizontal = 16.dp),
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
                 verticalArrangement = Arrangement.Top
             ) {
-                Log.e("ShowDetailContent", "Genres: ${episode.show.genres}")
                 episode.show.genres.forEach { genre ->
                     SuggestionChip(
                         enabled = false,
@@ -95,28 +130,30 @@ fun ShowDetailContent(episode: Episode) {
         Spacer(modifier = Modifier.height(16.dp))
         Text(
             modifier = Modifier.padding(horizontal = 16.dp),
-            text = "Airing time",
+            text = stringResource(id = R.string.airing_time),
             style = MaterialTheme.typography.titleMedium,
             color = Color.White
         )
         Spacer(modifier = Modifier.height(8.dp))
-        Row(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+                .padding(start = 16.dp, end = 16.dp, bottom = 24.dp)
         ) {
-            Column {
-                Text(
-                    text = "${episode.show.schedule.days.joinToString(", ")} ${episode.show.schedule.time}",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = Color.White
-                )
-                Text(
-                    text = "Season ${episode.season}, Episode ${episode.number}",
-                    style = MaterialTheme.typography.bodySmall,
-                    color = Color(0xFF9494C7)
-                )
-            }
+            Text(
+                text = "${episode.show.schedule.days.joinToString(", ")} at ${episode.show.schedule.time}",
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.White
+            )
+            Text(
+                text = stringResource(
+                    id = R.string.season_episode_format,
+                    episode.season,
+                    episode.number
+                ),
+                style = MaterialTheme.typography.bodySmall,
+                color = Color(0xFF9494C7)
+            )
         }
     }
 }
